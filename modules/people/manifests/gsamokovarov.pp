e8f0c753f3a0370048a70fd9683993e5cd730ae8
Genadi Samokovarov <gsamokovarov@gmail.com>
2014-02-23 19:36:03 +0200

    Don't show all this crap on the desktop
    Geezus, OSX! You really, like to display the .DS_Store on the desktop?

--- modules/people/manifests/gsamokovarov.pp
+++ modules/people/manifests/gsamokovarov.pp
@@ -87,10 +87,9 @@ class people::gsamokovarov {
   class { 'osx::dock::position': position => left }
   class { 'osx::dock::icon_size': size => 62 }
 
-  include osx::finder::show_all_on_desktop
+  include osx::finder::show_removable_media_on_desktop
   include osx::finder::empty_trash_securely
   include osx::finder::unhide_library
-  include osx::finder::show_hidden_files
   include osx::finder::enable_quicklook_text_selection
 
   include osx::universal_access::ctrl_mod_zoom
